73129d04d771aefbeeffafd804fc23e8f6d0b3e9
Martin Chapman <contact@martinchapman.co.uk>
2024-05-27 04:16:39 +0100

    Update made by Phenoflow

---
+++ python/upper-gastro-intestinal-malignancy-stomach---primary.py
@@ -0,0 +1,25 @@
+# Ellen Wright, Peter T Schofield, Paul Seed, Mariam Molokhia, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"B11..00","system":"readv2"},{"code":"B11z.00","system":"readv2"},{"code":"B114.00","system":"readv2"},{"code":"B11y100","system":"readv2"},{"code":"B11y000","system":"readv2"},{"code":"B113.00","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('upper-gastro-intestinal-malignancy-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["upper-gastro-intestinal-malignancy-stomach---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["upper-gastro-intestinal-malignancy-stomach---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["upper-gastro-intestinal-malignancy-stomach---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
